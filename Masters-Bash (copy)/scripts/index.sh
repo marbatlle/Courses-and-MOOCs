e8d2100540205693a287eb57a2d1b336f78d7f58
marbatlle <marbatlleperez@gmail.com>
2021-03-09 11:41:38 +0100

    edit

--- Masters-Bash (copy)/scripts/index.sh
+++
@@ -1,9 +0,0 @@
-# The STAR command is provided for you. You should replace the parts surrounded by "<>" and uncomment it.
-mkdir -p $2
-if [ ! -f $2/genomeParameters.txt ]
-then
-   STAR --runThreadN 4 --runMode genomeGenerate --genomeDir $2 --genomeFastaFiles $1 --genomeSAindexNbases 9
-   echo "Contaminant File Indexing COMPLETED"
-else
-   echo "PROCESS EXITED. Contaminant file Indexing OUTPUT ALREADY EXISTS"
-fi
--- Masters-Bash (copy)/scripts/merge_fastqs.sh
+++
@@ -1,3 +0,0 @@
-# Merge Fastqs
-mkdir -p out/merged
-cat $1/$3*.fastq.gz > $2/$3.fastq.gz
